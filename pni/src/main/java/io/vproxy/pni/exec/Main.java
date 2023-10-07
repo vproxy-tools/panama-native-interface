204d320d051b40ee6a4f1a1a1f3a2063e43776ca
wkgcass <wkgcass@hotmail.com>
2023-10-08 02:15:41 +0800

    begin 21.0.0.16-dev

--- pni/src/main/java/io/vproxy/pni/exec/Main.java
+++ pni/src/main/java/io/vproxy/pni/exec/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static final String GRAAL_GEN_VERSION;
 
     static {
-        final String _VERSION = "21.0.0.15"; // _THE_VERSION_
+        final String _VERSION = "21.0.0.16-dev"; // _THE_VERSION_
         final String _JAVA_GEN_VERSION = "21.0.0.15";
         final String _C_GEN_VERSION = "21.0.0.14";
         final String _GRAAL_GEN_VERSION = "21.0.0.15";
