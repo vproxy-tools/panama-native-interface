2afe33158f72ed4aed72ff72164b4145c38058a2
wkgcass <wkgcass@hotmail.com>
2023-10-09 21:33:40 +0800

    [sample] generate sample feature

--- sample/src/main/java/io/vproxy/pni/sample/Feature.java
+++ sample/src/main/java/io/vproxy/pni/sample/Feature.java
@@ -17,18 +17,30 @@ public class Feature implements org.graalvm.nativeimage.hosted.Feature {
         /* Java_io_vproxy_pni_sample_NativeFunctions_openIPv4TcpSocket */
         RuntimeForeignAccess.registerForDowncall(PanamaUtils.buildFunctionDescriptor(), Linker.Option.isTrivial());
         RuntimeReflection.registerAllConstructors(java.io.IOException.class);
+        for (var CONS : java.io.IOException.class.getConstructors()) {
+            RuntimeReflection.register(CONS);
+        }
 
         /* Java_io_vproxy_pni_sample_NativeFunctions_bindIPv4 */
         RuntimeForeignAccess.registerForDowncall(PanamaUtils.buildFunctionDescriptor(int.class /* fd */, int.class /* ipv4HostOrder */, int.class /* port */), Linker.Option.isTrivial());
         RuntimeReflection.registerAllConstructors(java.io.IOException.class);
+        for (var CONS : java.io.IOException.class.getConstructors()) {
+            RuntimeReflection.register(CONS);
+        }
 
         /* Java_io_vproxy_pni_sample_NativeFunctions_listen */
         RuntimeForeignAccess.registerForDowncall(PanamaUtils.buildFunctionDescriptor(int.class /* fd */, int.class /* n */), Linker.Option.isTrivial());
         RuntimeReflection.registerAllConstructors(java.io.IOException.class);
+        for (var CONS : java.io.IOException.class.getConstructors()) {
+            RuntimeReflection.register(CONS);
+        }
 
         /* Java_io_vproxy_pni_sample_NativeFunctions_accept */
         RuntimeForeignAccess.registerForDowncall(PanamaUtils.buildFunctionDescriptor(int.class /* fd */));
         RuntimeReflection.registerAllConstructors(java.io.IOException.class);
+        for (var CONS : java.io.IOException.class.getConstructors()) {
+            RuntimeReflection.register(CONS);
+        }
 
         /* Java_io_vproxy_pni_sample_NativeFunctions_close */
         RuntimeForeignAccess.registerForDowncall(PanamaUtils.buildFunctionDescriptor(int.class /* fd */), Linker.Option.isTrivial());
@@ -36,15 +48,24 @@ public class Feature implements org.graalvm.nativeimage.hosted.Feature {
         /* Java_io_vproxy_pni_sample_NativeFunctions_write */
         RuntimeForeignAccess.registerForDowncall(PanamaUtils.buildFunctionDescriptor(int.class /* fd */, MemorySegment.class /* mem */, int.class /* off */, int.class /* len */));
         RuntimeReflection.registerAllConstructors(java.io.IOException.class);
+        for (var CONS : java.io.IOException.class.getConstructors()) {
+            RuntimeReflection.register(CONS);
+        }
 
         /* Java_io_vproxy_pni_sample_NativeFunctions_read */
         RuntimeForeignAccess.registerForDowncall(PanamaUtils.buildFunctionDescriptor(int.class /* fd */, MemorySegment.class /* mem */, int.class /* off */, int.class /* len */));
         RuntimeReflection.registerAllConstructors(java.io.IOException.class);
+        for (var CONS : java.io.IOException.class.getConstructors()) {
+            RuntimeReflection.register(CONS);
+        }
 
         /* Java_io_vproxy_pni_sample_SampleFunctions_read */
-        RuntimeForeignAccess.registerForDowncall(PanamaUtils.buildFunctionDescriptor(int.class /* fd */, io.vproxy.pni.sample.MBuf.LAYOUT.getClass() /* buf */));
+        RuntimeForeignAccess.registerForDowncall(PanamaUtils.buildFunctionDescriptor(int.class /* fd */, MemoryLayout.class /* io.vproxy.pni.sample.MBuf.LAYOUT.getClass() */ /* buf */));
         RuntimeReflection.registerAllConstructors(java.io.IOException.class);
+        for (var CONS : java.io.IOException.class.getConstructors()) {
+            RuntimeReflection.register(CONS);
+        }
     }
 }
 // metadata.generator-version: pni test
-// sha256:3a4a7ede2a5e87559448b9f4c3db69b99513eb286b76b5caf9c82a04eb7d9f4c
+// sha256:087eb3423d7f1431688754f14a066d27a409cb23d424f643ed323d5346382b3f
